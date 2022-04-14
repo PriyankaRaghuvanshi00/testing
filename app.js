bdc4e7aa357d9428a0ed61f5f97f2f4539bbc7bc
PriyankaRaghuvanshi00 <76490340+PriyankaRaghuvanshi00@users.noreply.github.com>
2022-04-14 16:24:12 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyparser = require('body-parser');
+
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended: false }))
+app.get('/', (req, res, next) => {
+   res.json({ name: "priyanka", enroll: 176 })
+})
+
+app.post('/user', (req, res, next) => {
+   const name = req.body.name, enroll = req.body;
+})
+
+app.get('/user', (req, res, next) => {
+   res.json({ msg: "hello" });
+})
+app.listen(3000, () => {
+   console.log("listening 3000");
+});
