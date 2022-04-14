b6e2f3bc23135c63aec825c74d2bf256132b34b7
PriyankaRaghuvanshi00 <76490340+PriyankaRaghuvanshi00@users.noreply.github.com>
2022-04-14 16:33:52 +0530

    first

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 var bodyparser = require('body-parser');
-
+var port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }))
 app.get('/', (req, res, next) => {
@@ -10,12 +10,12 @@ app.get('/', (req, res, next) => {
 })
 
 app.post('/user', (req, res, next) => {
-
+   const name = req.body.name, enroll = req.body;
 })
 
 app.get('/user', (req, res, next) => {
    res.json({ msg: "hello" });
 })
-app.listen(3000, () => {
+app.listen(port, () => {
    console.log("listening 3000");
 });
